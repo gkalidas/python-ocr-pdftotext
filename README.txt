8dcf68f10bff7e2f618c8f8fef196c98cc095b3d
glondhe <ganesh.londhe@crenexa.com>
2019-12-14 01:32:07 +0530

    calculating number of pages to be converted into jpg

--- README.txt
+++ README.txt
@@ -1,4 +1,9 @@
+LIBRARIES
+NAME            DESCRIPTION                 INSTALLATION COMMAND
+pyPdf           working with python         pip install pyPdf==1.13
+
+
+
 ERROR CODE     DESCRIPTION                  SOLUTION
     1          convert not authorized       https://alexvanderbist.com/posts/2018/fixing-imagick-error-unauthorized
 
-    
--- ocr.py
+++ ocr.py
@@ -3,6 +3,10 @@ import os
 import subprocess
 from os import listdir
 from os.path import isfile, join
+
+#getting number of pages in a pdf
+from PyPDF2 import PdfFileReader
+from subprocess import check_output
 # working with arguements
 
 print("Arguements List :" ,str(sys.argv))
@@ -21,22 +25,28 @@ all_input_file_path = "./input"
 only_files = [f for f in listdir(all_input_file_path) if isfile(join(all_input_file_path, f))]
 #print("There are {} files in the list ".format(len(only_files)))
 
+def get_num_pages(pdf_path):
+    output = check_output(["pdfinfo", pdf_path]).decode()
+    pages_line = [line for line in output.splitlines() if "Pages:" in line][0]
+    num_pages = int(pages_line.split(":")[1])
+    return num_pages
+
 for index, input_file in enumerate(only_files):
     #print("File {}: {} ".format(index,input_file))
 
-    #converting each file into jpg
-    #convert -density [150-300] GaneshLondhe.pdf GaneshLondhe.jpg
-
     input_file_path = all_input_file_path + "/" +  input_file
+    a = get_num_pages(input_file_path)
+    print("Number of pages ", a)
+    #print("There are {} pages in {}".format(number_of_pages(input_file_path),input_file_path))
     #print(" Input file path : " ,input_file_path)
 
-    head, tail = os.path.splitext(input_file)
-    output_file_name = "./temp/" + head + '.jpg'
+    # head, tail = os.path.splitext(input_file)
+    # output_file_name = "./temp/" + head + '.jpg'
 
-    #ERROR 1
-
-    command = "convert -density 150 "+ input_file_path +" "+ output_file_name +""
-    subprocess.run([command], shell = True)
+    # convert pdf to jpg for text recognization
+    #You may encounter ERROR 1 here
+    #command = "convert -density 150 "+ input_file_path +" "+ output_file_name +""
+    #subprocess.run([command], shell = True)
 
 # output_file_path = "./output/output"
 
