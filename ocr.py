cd4ddefc51a86205b6cd7422ba37d56e4923e61a
glondhe <ganesh.londhe@crenexa.com>
2019-12-13 23:30:10 +0530

    reading all file names, storing into a list and iterating over them

--- ocr.py
+++ ocr.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import subprocess
+from os import listdir
+from os.path import isfile, join
 # working with arguements
 
 print("Arguements List :" ,str(sys.argv))
@@ -9,10 +11,22 @@ print("Arguements List :" ,str(sys.argv))
 #getopt.getopt(args, options, [long_options])
 #print(__file__)
 
-input_file_path = "./input/output-1.jpg"
-output_file_path = "./output/output"
+input_file_path = "./input"
 
-command = "tesseract "+ input_file_path +" "+ output_file_path +" -l eng --oem 1 --psm 3"
+#extracting file-name from path
+# head, tail = os.path.split(input_file_path)
+# print("File name : ",tail)
 
-print(command)
-subprocess.run([command], shell= True)
+#getting only all the files into a list
+only_files = [f for f in listdir(input_file_path) if isfile(join(input_file_path, f))]
+
+print("There are {} files in the list ".format(len(only_files)))
+
+for i, x in enumerate(only_files):
+    print("File {}: {} ".format(i,x))
+
+    #converting each file into jpg
+
+# output_file_path = "./output/output"
+# command = "tesseract "+ input_file_path +" "+ output_file_path +" -l eng --oem 1 --psm 3"
+# subprocess.run([command], shell= True)
