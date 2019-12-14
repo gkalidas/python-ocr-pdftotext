e47d001db4f4d00e24771167864f2429d006ee45
glondhe <ganesh.londhe@crenexa.com>
2019-12-14 09:34:32 +0530

    counted total files and number of pages in it

--- ocr.py
+++ ocr.py
@@ -5,11 +5,10 @@ from os import listdir
 from os.path import isfile, join
 
 #getting number of pages in a pdf
-from PyPDF2 import PdfFileReader
 from subprocess import check_output
 # working with arguements
 
-print("Arguements List :" ,str(sys.argv))
+#print("Arguements List :" ,str(sys.argv))
 # getting file name and path of the file
 
 #getopt.getopt(args, options, [long_options])
@@ -26,18 +25,28 @@ only_files = [f for f in listdir(all_input_file_path) if isfile(join(all_input_f
 #print("There are {} files in the list ".format(len(only_files)))
 
 def get_num_pages(pdf_path):
+
+    # returns the number of pages from a given file
+
     output = check_output(["pdfinfo", pdf_path]).decode()
     pages_line = [line for line in output.splitlines() if "Pages:" in line][0]
     num_pages = int(pages_line.split(":")[1])
     return num_pages
 
-for index, input_file in enumerate(only_files):
-    #print("File {}: {} ".format(index,input_file))
+total_pages = 0; total_files = len(only_files)
 
+for input_file in only_files:
     input_file_path = all_input_file_path + "/" +  input_file
     a = get_num_pages(input_file_path)
-    print("Number of pages ", a)
-    #print("There are {} pages in {}".format(number_of_pages(input_file_path),input_file_path))
+    total_pages += a
+    print("Input files {} contains {} pages.".format(input_file,a))
+
+print("There are {} files and contains {} pages ".format(total_files,total_pages))
+
+#for index, input_file in enumerate(only_files):
+    #print("File {}: {} ".format(index,input_file))
+
+    #input_file_path = all_input_file_path + "/" +  input_file
     #print(" Input file path : " ,input_file_path)
 
     # head, tail = os.path.splitext(input_file)
